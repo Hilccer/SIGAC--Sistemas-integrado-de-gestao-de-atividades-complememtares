4d8cc9dc6358a98fe317de5a8dcf2503b967ba98
Hilccer <hilccer@gmail.com>
2026-04-06 09:24:01 -0300

    Create SigacApplication.java

---
+++ SigacApplication.java
@@ -0,0 +1,11 @@
+package com.sigac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigacApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SigacApplication.class, args);
+    }
+}
